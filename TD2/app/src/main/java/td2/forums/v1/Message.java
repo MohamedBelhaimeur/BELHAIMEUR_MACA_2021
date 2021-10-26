8de223925373311455baf6b0e22a3463decbb03f
Mohamed Belhaimeur <belhaim789@gmail.com>
2021-10-26 12:37:35 +0200

    ..

--- TD2/app/src/main/java/td2/forums/v1/Message.java
+++ TD2/app/src/main/java/td2/forums/v1/Message.java
@@ -10,7 +10,7 @@ public class Message {
     public Message(String topic,String contenu){
         this.topic=topic;
         this.contenu=contenu;
-      
+
     }
     public String sujet(){
         return this.topic;
